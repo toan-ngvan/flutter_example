f0bd979f8e1708250611b48c543255e52d0551d9
Toan Nguyen V <toan.ngvan94@gmail.com>
2019-11-13 11:52:33 +0700

    to be continue

--- lib/pages/stepByStepExample.dart
+++ lib/pages/stepByStepExample.dart
@@ -10,16 +10,65 @@ class _StepByStepExampleState extends State<StepByStepExample> {
 
   @override
   Widget build(BuildContext context) {
-    print('============');
-    print('abc');
-    print('============');
     return Scaffold(
       appBar: AppBar(
         title: Text('Step by step example'),
         centerTitle: true,
       ),
-      body: Row(
-        children: _renderSteps(4),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IntrinsicWidth(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Expanded(
+                    child: Align(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text('Step 1'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text('Stepss 1'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text('Stepssss 1'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Text('Stepssssss 1'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: _renderSteps(4),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -36,7 +85,7 @@ class _StepByStepExampleState extends State<StepByStepExample> {
     List<Widget> a = [];
     a.add(_renderStep(1));
     for (var i = 2; i <= step; i++) {
-      a.add(_renderConnectLine(i));
+      a.add(_renderConnectLine(i, 50));
       a.add(_renderStep(i));
     }
     return a.toList();
@@ -52,7 +101,6 @@ class _StepByStepExampleState extends State<StepByStepExample> {
       ),
       height: 30,
       width: 30,
-      // padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.elliptical(30, 30)),
         color: i <= finishStep ? Colors.red : Colors.grey,
@@ -60,10 +108,10 @@ class _StepByStepExampleState extends State<StepByStepExample> {
     );
   }
 
-  _renderConnectLine(int i) {
+  _renderConnectLine(int i, double width) {
     return Container(
       height: 5,
-      width: 20,
+      width: width,
       color: i <= finishStep ? Colors.red : Colors.grey,
     );
   }
